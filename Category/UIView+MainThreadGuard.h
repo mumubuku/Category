abd4acc0ed64377d20fc816e77bfa9c5ce347213
tianyunzhao <tianyunzhao@soullink.com.cn>
2019-09-20 15:03:30 +0800

    增加主线程uicheck分类

---
+++ Category/UIView+MainThreadGuard.h
@@ -0,0 +1,17 @@
+//
+//  UIView+MainThreadGuard.h
+//  pick
+//
+//  Created by soda on 2019/8/16.
+//  Copyright © 2019 Soulink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (MainThreadGuard)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Category/UIView+MainThreadGuard.m
@@ -0,0 +1,73 @@
+//
+//  UIView+MainThreadGuard.m
+//  pick
+//
+//  Created by soda on 2019/8/16.
+//  Copyright © 2019 Soulink. All rights reserved.
+//
+
+#import "UIView+MainThreadGuard.h"
+#import <objc/runtime.h>
+
+static inline void swizzling_exchangeMethod(Class clazz ,SEL originalSelector, SEL swizzledSelector){
+    Method originalMethod = class_getInstanceMethod(clazz, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(clazz, swizzledSelector);
+    
+    BOOL success = class_addMethod(clazz, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+    if (success) {
+        class_replaceMethod(clazz, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+    } else {
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
+}
+
+@implementation UIView (MainThreadGuard)
+
++(void)load
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        SEL needsLayoutOriginalSelector = @selector(setNeedsLayout);
+        SEL needsLayoutSwizzleSelector  = @selector(guard_setNeedsLayout);
+        swizzling_exchangeMethod(self, needsLayoutOriginalSelector,needsLayoutSwizzleSelector);
+        
+        SEL needsDisplaOriginalSelector = @selector(setNeedsDisplay);
+        SEL needsDisplaSwizzleSelector  = @selector(guard_setNeedsDisplay);
+        swizzling_exchangeMethod(self, needsDisplaOriginalSelector,needsDisplaSwizzleSelector);
+        
+        SEL needsDisplayInRectOriginalSelector = @selector(setNeedsDisplayInRect:);
+        SEL needsDisplayInRectSwizzleSelector  = @selector(guard_setNeedsDisplayInRect:);
+        swizzling_exchangeMethod(self, needsDisplayInRectOriginalSelector,needsDisplayInRectSwizzleSelector);
+        
+    });
+}
+
+- (void)guard_setNeedsLayout
+{
+    [self UIMainThreadCheck];
+    [self guard_setNeedsLayout];
+}
+
+- (void)guard_setNeedsDisplay
+{
+    [self UIMainThreadCheck];
+    [self guard_setNeedsDisplay];
+}
+
+
+- (void)guard_setNeedsDisplayInRect:(CGRect)rect
+{
+    [self UIMainThreadCheck];
+    [self guard_setNeedsDisplayInRect:rect];
+}
+
+- (void)UIMainThreadCheck
+{
+    NSString *desc = [NSString stringWithFormat:@"%@", self.class];
+    NSAssert([NSThread isMainThread], desc);
+}
+
+@end
+
+
